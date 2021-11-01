e36daa3329b7cb215d9a464aaa3f7f808bded93d
--global <--global>
2021-11-01 15:44:41 -0600

    added hello world javascript

---
+++ scripts/Hello-world.js
@@ -0,0 +1,11 @@
+ // this a comment - single line
+ // this the hello world program 
+ /*
+
+ This is a block comment a comment without line limits 
+ like this
+
+ */
+
+
+ console.log("Hello world!")

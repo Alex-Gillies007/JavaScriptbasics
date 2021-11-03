36b1c2cb495e39954cbf9e061e0ec63224c990dc
--global <--global>
2021-11-03 16:30:09 -0600

    All my changes

--- scripts/Hello-world.js
+++ scripts/Hello-world.js
@@ -9,3 +9,12 @@
 
 
  console.log("Hello world!")
+ 
+ let firstName = "Alex";
+
+ console.log("Hello world my name is %s", firstName);
+
+ let age = 14
+
+ console.log("Hello world my age is %d", age);
+ 
---
+++ scripts/variables.js
@@ -0,0 +1,30 @@
+// variables are named containers for stuff
+
+// let is a keyword to declare variables
+// age is the name of my variable
+//16 is the value to variable age
+
+let age = 14; // the data type of age is a number
+
+let name = "Alex Gillies"; // the data type of fullname is string
+
+let isactive = true; // the data type is isactive is boolean (true or false)
+
+age = false; // age is now a boolean
+
+const MAX_AGE = 200;
+
+var visibleEverywhere = true;
+
+var leg = 16;
+
+let bar = 10;
+
+console.log(leg, bar);
+
+{
+	let bar = 20;
+
+	var leg = 32;
+	console.log(foo, bar);
+}
